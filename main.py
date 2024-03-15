e9b2811c99b1a356b9ae8519d65b8a3dbde354ea
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-03-15 16:27:24 +0100

    python start practice

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Hello World")
+
+print(print)
+
+name = 'Artem'
+print(name.upper())
+
+print(print("g"))
+
+print(dir())
+
+
+name = input("Enter your name: ")
+print(name)
+
+print(name.capitalize())
+
+print(dir(name))
