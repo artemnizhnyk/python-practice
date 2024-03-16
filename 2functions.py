97384a9a476421f6c18b5a7cc82bf87de3587575
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-03-16 18:07:08 +0100

    practice with functions

---
+++ 2functions.py
@@ -0,0 +1,28 @@
+def my_name(name):
+    print(name)
+
+
+my_name("Artem")
+
+print("----------------------")
+
+
+def hello(word):
+    print(word)
+
+
+hello("Hello world")
+
+print("----------------------")
+
+
+def sum_nums(a, b):
+    return a + b
+
+
+sum = sum_nums(10, 5)
+print(sum)
+
+print("----------------------")
+
+
