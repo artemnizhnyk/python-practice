7c0db3fc07f9789ff4f693a17e828f93b225ee2b
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-03-18 23:33:08 +0100

    practice with booleans

---
+++ 6booleans.py
@@ -0,0 +1,13 @@
+is_authorized = True
+print(is_authorized)
+print(type(is_authorized))
+
+print(100 > 4)
+print(100 < 4)
+print(-5 < 4)
+
+print("--------------------")
+
+print('Long' < 'Long string')
+print('a' > 'z')
+print([1, 2, 3] == [1, 2, 3])
