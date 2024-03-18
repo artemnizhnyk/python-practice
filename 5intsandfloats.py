5b8ee4bc674ea16624e93192da4180b43776d053
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-03-18 21:06:32 +0100

    practice with ints and floats

---
+++ 5intsandfloats.py
@@ -0,0 +1,38 @@
+some_int = 50
+
+print(type(some_int))
+
+user_input = input("Enter any number: ")
+any_num = int(user_input)
+
+print(any_num)
+print(type(any_num))
+
+base = 5
+power = 3
+
+print(pow(base, power))
+
+print("-------------------------")
+
+any_float = 5.1
+print(any_float)
+print(type(float))
+
+any_float = '5.6'
+print(any_float)
+
+any_float = '6'
+print(any_float)
+
+any_float = 6.6
+print(round(any_float))
+
+print("-------------------------")
+
+complex_a = 3 + 5j
+complex_b = 3 + 7j
+
+sum = complex_a + complex_b
+
+print(sum)
