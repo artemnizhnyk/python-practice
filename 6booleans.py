91b955360ab6b031e2803d7b9d099d274c5a16a6
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-03-20 11:58:46 +0100

    aditional practice with booleans

--- 6booleans.py
+++ 6booleans.py
@@ -11,3 +11,19 @@ print("--------------------")
 print('Long' < 'Long string')
 print('a' > 'z')
 print([1, 2, 3] == [1, 2, 3])
+
+print("--------------------")
+
+db_is_available = False
+
+print(db_is_available)
+print(type(db_is_available))
+
+db_is_available = True
+print(db_is_available)
+
+print(bool(10))
+print(bool('abc'))
+print(bool([]))
+print(bool([1, 2]))
+print(bool(None))
